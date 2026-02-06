02f13d58c1b84d69b0eaca9f695d4ee181bd20d5
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2026-02-06 11:58:16 +0100

    Added all classes

---
+++ ingredient.py
@@ -0,0 +1,7 @@
+class Ingredient:
+
+    def __init__(self, naam: str, hoeveelheid: float, eenheid: str):
+        self.__naam__ = naam
+        self.__hoeveelheid__ = hoeveelheid
+        self.__eenheid__ = eenheid
+   
---
+++ stap.py
@@ -0,0 +1,3 @@
+class Stap:
+    def __init__(self, beschrijving: str):
+        self.__beschrijving__ = beschrijving
